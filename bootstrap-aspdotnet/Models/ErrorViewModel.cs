73a864b470df4e64a714b3bd0f319ddaf11c56c0
Angel <arivalladares2.0@gmail.com>
2022-11-14 02:59:20 -0600

    Add project files.

---
+++ bootstrap-aspdotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bootstrap_aspdotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
